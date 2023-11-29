561e86eb776ff80270a6495f99b0c0154aa4d332
Anton Dubovik <anton_dubovik@epam.com>
2023-11-29 13:20:13 +0000

    fix: represent prices as strings in the listing (#50)

--- src/main/java/com/epam/aidial/core/config/Pricing.java
+++ src/main/java/com/epam/aidial/core/config/Pricing.java
@@ -1,10 +1,16 @@
 package com.epam.aidial.core.config;
 
+import com.epam.aidial.core.util.DoubleStringDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 
 @Data
 public class Pricing {
     private String unit;
-    private Double prompt;
-    private Double completion;
+
+    @JsonDeserialize(using = DoubleStringDeserializer.class)
+    private String prompt;
+
+    @JsonDeserialize(using = DoubleStringDeserializer.class)
+    private String completion;
 }
