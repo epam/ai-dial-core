b49b0c3e8a84856bbfa651d910b28ae8b11081f0
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2023-12-28 12:47:55 +0300

    fix: Base assistant is shown in the listing API response #103

--- src/test/java/com/epam/aidial/core/controller/DeploymentPostControllerTest.java
+++ src/test/java/com/epam/aidial/core/controller/DeploymentPostControllerTest.java
@@ -40,12 +40,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.assertArg;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
