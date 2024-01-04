6c2cb9b95572d9d9cc0849a479826060073b64f0
Oleksii-Klimov <133792808+Oleksii-Klimov@users.noreply.github.com>
2024-01-04 16:07:33 +0000

    feat: populate addon name in assistant request (#109)

--- src/main/java/com/epam/aidial/core/controller/DeploymentPostController.java
+++ src/main/java/com/epam/aidial/core/controller/DeploymentPostController.java
@@ -400,7 +400,7 @@ public class DeploymentPostController {
                 }
 
                 String url = addon.getEndpoint();
-                addons.addObject().put("url", url);
+                addons.addObject().put("url", url).put("name", name);
                 if (addon.getToken() != null && !addon.getToken().isBlank()) {
                     headers.put("x-addon-token-" + addonIndex, addon.getToken());
                 }
