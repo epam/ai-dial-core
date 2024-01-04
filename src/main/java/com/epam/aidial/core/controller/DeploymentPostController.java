99ec220b911f05184aa84d9049ab42585b4a718f
Alexey Klimov <alexey_klimov@epam.com>
2024-01-04 15:51:42 +0000

    Update same entry.

--- src/main/java/com/epam/aidial/core/controller/DeploymentPostController.java
+++ src/main/java/com/epam/aidial/core/controller/DeploymentPostController.java
@@ -400,8 +400,7 @@ public class DeploymentPostController {
                 }
 
                 String url = addon.getEndpoint();
-                addons.addObject().put("url", url);
-                addons.addObject().put("name", name);
+                addons.addObject().put("url", url).put("name", name);
                 if (addon.getToken() != null && !addon.getToken().isBlank()) {
                     headers.put("x-addon-token-" + addonIndex, addon.getToken());
                 }
