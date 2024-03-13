e1f7ee41f5feee1d55d8e1127d377ef44bfb8854
Aliaksandr Stsiapanay <aliaksandr.stsiapanay@gmail.com>
2024-03-13 10:56:43 +0100

    fix: run processing request body async in order to prevent event thread blocking (#274)
    Co-authored-by: Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>

--- src/main/java/com/epam/aidial/core/controller/DeploymentPostController.java
+++ src/main/java/com/epam/aidial/core/controller/DeploymentPostController.java
@@ -133,7 +133,10 @@ public class DeploymentPostController {
         proxy.getTokenStatsTracker().startSpan(context);
 
         context.getRequest().body()
-                .onSuccess(this::handleRequestBody)
+                .onSuccess(body -> proxy.getVertx().executeBlocking(() -> {
+                    handleRequestBody(body);
+                    return null;
+                }))
                 .onFailure(this::handleRequestBodyError);
     }
 
