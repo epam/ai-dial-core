e0b98d530c841d393d6da077dbd9ca22f8138dd0
Maxim <gmaksim2012@gmail.com>
2024-05-15 19:10:04 +0200

    Revert "fix: Deployment names with colon symbols couldn't be resolved #332 (#…"
    This reverts commit ee252dcc550bc1c058b4bee7032539c5ec95eb79.

--- src/main/java/com/epam/aidial/core/util/UrlUtil.java
+++ src/main/java/com/epam/aidial/core/util/UrlUtil.java
@@ -1,39 +1,31 @@
 package com.epam.aidial.core.util;
 
-import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
-import org.apache.commons.codec.net.PercentCodec;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.charset.Charset;
 
 @UtilityClass
 public class UrlUtil {
 
-    private static final PercentCodec CODEC = new PercentCodec(" ?/#".getBytes(Charset.defaultCharset()), false);
-
-    @SneakyThrows
     public String encodePath(String path) {
-        return new String(CODEC.encode(path.getBytes(Charset.defaultCharset())));
+        try {
+            URI uri = new URI(null, null, path, null);
+            return uri.toASCIIString();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public String decodePath(String path) {
-        return decodePath(path, true);
-    }
-
-    @SneakyThrows
-    public String decodePath(String path, boolean checkUri) {
-        if (checkUri) {
-            try {
-                URI uri = new URI(path);
-                if (uri.getRawFragment() != null || uri.getRawQuery() != null) {
-                    throw new IllegalArgumentException("Wrong path provided " + path);
-                }
-            } catch (URISyntaxException e) {
-                throw new RuntimeException(e);
+        try {
+            URI uri = new URI(path);
+            if (uri.getRawFragment() != null || uri.getRawQuery() != null) {
+                throw new IllegalArgumentException("Wrong path provided " + path);
             }
+            return uri.getPath();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
         }
-        return new String(CODEC.decode(path.getBytes(Charset.defaultCharset())));
     }
 }
--- src/test/java/com/epam/aidial/core/util/UrlUtilTest.java
+++ src/test/java/com/epam/aidial/core/util/UrlUtilTest.java
@@ -22,7 +22,6 @@ public class UrlUtilTest {
         assertEquals("fo$l+=d,e#r 1", UrlUtil.decodePath("fo$l+=d,e%23r%201"));
         assertEquals("fo$l+=d,e#r 1", UrlUtil.decodePath("fo%24l%2B%3Dd%2Ce%23r%201"));
         assertEquals("файл.txt", UrlUtil.decodePath("%D1%84%D0%B0%D0%B9%D0%BB.txt"));
-        assertEquals("echo:1", UrlUtil.decodePath("echo:1"));
         assertThrows(IllegalArgumentException.class, () -> UrlUtil.decodePath("/folder1/file#5.txt"));
         assertThrows(IllegalArgumentException.class, () -> UrlUtil.decodePath("/folder1/q?file=5.txt"));
     }
