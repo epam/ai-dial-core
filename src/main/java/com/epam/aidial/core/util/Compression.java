b00f58dc715eb86df1146834c22db15c08233b3c
Alexey Klimov <alexey_klimov@epam.com>
2024-07-18 16:25:33 +0100

    Catch zip exception in correct place.

--- src/main/java/com/epam/aidial/core/util/Compression.java
+++ src/main/java/com/epam/aidial/core/util/Compression.java
@@ -34,18 +34,19 @@ public class Compression {
             throw new IllegalArgumentException("Unsupported compression: " + type);
         }
 
-        return new GZIPInputStream(input);
+        try {
+            return new GZIPInputStream(input);
+        } catch (ZipException e) {
+            // special case for GCP cloud storage, due to jclouds bug https://issues.apache.org/jira/projects/JCLOUDS/issues/JCLOUDS-1633
+            log.warn("Failed to decompress provided input: {}", e.getMessage());
+            return input;
+        }
     }
-
 
     @SneakyThrows
     public byte[] decompress(String type, byte[] input) {
         try (InputStream decompressed = decompress(type, new ByteArrayInputStream(input))) {
             return decompressed.readAllBytes();
-        } catch (ZipException e) {
-            // special case for GCP cloud storage, due to jclouds bug https://issues.apache.org/jira/projects/JCLOUDS/issues/JCLOUDS-1633
-            log.warn("Failed to decompress provided input: {}", e.getMessage());
-            return input;
         }
     }
 }
