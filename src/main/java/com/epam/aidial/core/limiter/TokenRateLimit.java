feb96d71cf0e6978d5790814040edb30a6f6cec4
Aleksandr Chikovani <real.rp@gmail.com>
2024-08-02 03:23:23 -0400

    chore: remove linebreaks at multiline logs (#408)
    Co-authored-by: Aleksandr Chikovani <a@justrp.ru>
    Co-authored-by: Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>

--- src/main/java/com/epam/aidial/core/limiter/TokenRateLimit.java
+++ src/main/java/com/epam/aidial/core/limiter/TokenRateLimit.java
@@ -22,11 +22,7 @@ public class TokenRateLimit {
 
         boolean result = minuteTotal >= limit.getMinute() || dayTotal >= limit.getDay();
         if (result) {
-            String errorMsg = String.format("""
-                            Hit token rate limit:
-                             - minute limit: %d / %d tokens
-                             - day limit: %d / %d tokens
-                            """,
+            String errorMsg = String.format("Hit token rate limit. Minute limit: %d / %d tokens. Day limit: %d / %d tokens.",
                     minuteTotal, limit.getMinute(), dayTotal, limit.getDay());
             return new RateLimitResult(HttpStatus.TOO_MANY_REQUESTS, errorMsg);
         } else {
