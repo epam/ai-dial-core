417d2fb80fde3f07685a80f8b42b819a0027bd1b
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2024-09-26 18:52:54 +0300

    chore: remove unused import

--- src/main/java/com/epam/aidial/core/security/IdentityProvider.java
+++ src/main/java/com/epam/aidial/core/security/IdentityProvider.java
@@ -23,7 +23,6 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPublicKey;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
