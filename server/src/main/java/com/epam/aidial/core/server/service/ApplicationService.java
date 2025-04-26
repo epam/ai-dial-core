f8e19ab8e18ddddda8499e8e1568af36512f7657
Sergey Zinchenko <sergey.zinchenko.rnd@gmail.com>
2025-04-26 10:14:43 +0200

    fix: removed reformatting

--- server/src/main/java/com/epam/aidial/core/server/service/ApplicationService.java
+++ server/src/main/java/com/epam/aidial/core/server/service/ApplicationService.java
@@ -684,8 +684,8 @@ public class ApplicationService {
 
     private String encodeTargetFolder(ResourceDescriptor resource, String id) {
         String location = resource.getBucketLocation()
-                          + DEPLOYMENTS_NAME + ResourceDescriptor.PATH_SEPARATOR
-                          + id + ResourceDescriptor.PATH_SEPARATOR;
+                + DEPLOYMENTS_NAME + ResourceDescriptor.PATH_SEPARATOR
+                + id + ResourceDescriptor.PATH_SEPARATOR;
 
         String name = encryptionService.encrypt(location);
         return ResourceDescriptorFactory.fromDecoded(ResourceTypes.FILE, name, location, null).getUrl();
