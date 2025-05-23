f70c8a2d751cdac0772b3b1e70a29dc381184b17
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2025-05-23 11:01:34 +0300

    feat: Redesign core rate limit message error #823 (#825)

--- server/src/main/java/com/epam/aidial/core/server/limiter/RateLimitResult.java
+++ server/src/main/java/com/epam/aidial/core/server/limiter/RateLimitResult.java
@@ -2,6 +2,6 @@ package com.epam.aidial.core.server.limiter;
 
 import com.epam.aidial.core.storage.http.HttpStatus;
 
-public record RateLimitResult(HttpStatus status, String errorMessage, long replyAfterSeconds) {
-    public static final RateLimitResult SUCCESS = new RateLimitResult(HttpStatus.OK, null, -1);
+public record RateLimitResult(HttpStatus status, String errorMessage, String displayErrorMessage, long replyAfterSeconds) {
+    public static final RateLimitResult SUCCESS = new RateLimitResult(HttpStatus.OK, null, null, -1);
 }
