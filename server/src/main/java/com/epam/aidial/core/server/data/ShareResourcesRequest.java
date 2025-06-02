24520fb675220d198f2af1d11907888f6b85c396
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2025-06-02 18:26:13 +0300

    chore: set maxAcceptedUsers

--- server/src/main/java/com/epam/aidial/core/server/data/ShareResourcesRequest.java
+++ server/src/main/java/com/epam/aidial/core/server/data/ShareResourcesRequest.java
@@ -1,25 +1,12 @@
 package com.epam.aidial.core.server.data;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Data
 public class ShareResourcesRequest {
     Set<SharedResource> resources;
     InvitationType invitationType;
     int maxAcceptedUsers = Integer.MAX_VALUE;
-
-    @JsonCreator
-    public ShareResourcesRequest(
-            @JsonProperty("resources") Set<SharedResource> resources,
-            @JsonProperty("invitationType") InvitationType invitationType) {
-        this.resources = resources.stream()
-                .map(SharedResource::withReadIfNoPermissions)
-                .collect(Collectors.toSet());
-        this.invitationType = invitationType;
-    }
 }
