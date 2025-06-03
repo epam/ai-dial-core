38c534b258791cb416ba397b3a1356ff7ad42dd9
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2025-06-03 15:50:07 +0300

    test AWS blob storage

--- storage/src/main/java/com/epam/aidial/core/storage/blobstore/credential/AwsCredentialProvider.java
+++ storage/src/main/java/com/epam/aidial/core/storage/blobstore/credential/AwsCredentialProvider.java
@@ -2,7 +2,15 @@ package com.epam.aidial.core.storage.blobstore.credential;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSSessionCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceAsyncClientBuilder;
+import com.amazonaws.services.securitytoken.model.AssumeRoleRequest;
+import com.amazonaws.services.securitytoken.model.AssumeRoleResult;
 import org.jclouds.aws.domain.SessionCredentials;
 import org.jclouds.domain.Credentials;
 
@@ -24,14 +32,38 @@ public class AwsCredentialProvider implements CredentialProvider {
         if (credentials != null) {
             return credentials;
         }
-        AWSCredentials awsCredentials = providerChain.getCredentials();
-        if (awsCredentials instanceof AWSSessionCredentials awsSessionCredentials) {
-            return SessionCredentials.builder()
-                    .accessKeyId(awsSessionCredentials.getAWSAccessKeyId())
-                    .secretAccessKey(awsSessionCredentials.getAWSSecretKey())
-                    .sessionToken(awsSessionCredentials.getSessionToken()).build();
+        if (true) {
+            AWSCredentials awsCredentials = new BasicSessionCredentials(System.getenv("AWS_ACCESS_KEY_ID"),
+                    System.getenv("AWS_SECRET_ACCESS_KEY"),
+                    System.getenv("AWS_SESSION_TOKEN"));
+            AWSStaticCredentialsProvider provider = new AWSStaticCredentialsProvider(awsCredentials);
+            AWSSecurityTokenService stsClient = AWSSecurityTokenServiceAsyncClientBuilder.standard()
+                    .withCredentials(provider)
+                    .withRegion("eu-north-1")
+                    .build();
+
+            AssumeRoleRequest assumeRoleRequest = new AssumeRoleRequest().withDurationSeconds(3600)
+                    .withRoleArn("arn:aws:iam::725751206603:role/dial")
+                    .withRoleSessionName("CloudWatch_Session");
+
+            AssumeRoleResult assumeRoleResult = stsClient.assumeRole(assumeRoleRequest);
+            com.amazonaws.services.securitytoken.model.Credentials creds = assumeRoleResult.getCredentials();
+            credentials =  SessionCredentials.builder()
+                    .accessKeyId(creds.getAccessKeyId())
+                    .secretAccessKey(creds.getSecretAccessKey())
+                    .sessionToken(creds.getSessionToken()).build();
+            return credentials;
         } else {
-            return new Credentials(awsCredentials.getAWSAccessKeyId(), awsCredentials.getAWSSecretKey());
+            AWSCredentials awsCredentials = providerChain.getCredentials();
+            if (awsCredentials instanceof AWSSessionCredentials awsSessionCredentials) {
+                return SessionCredentials.builder()
+                        .accessKeyId(awsSessionCredentials.getAWSAccessKeyId())
+                        .secretAccessKey(awsSessionCredentials.getAWSSecretKey())
+                        .sessionToken(awsSessionCredentials.getSessionToken()).build();
+            } else {
+                return new Credentials(awsCredentials.getAWSAccessKeyId(), awsCredentials.getAWSSecretKey());
+            }
         }
     }
+
 }
