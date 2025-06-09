6c92dbe842d72c80acac1a019f308b111a9bf475
Alexander <i.am.v.alexander.v@gmail.com>
2025-06-09 20:45:43 +0400

    Refactor: extract hidden folder tests from `AccessServiceTest` to new `ResourceDescriptorTest` class for improved test separation.

--- server/src/test/java/com/epam/aidial/core/server/service/AccessServiceTest.java
+++ server/src/test/java/com/epam/aidial/core/server/service/AccessServiceTest.java
@@ -17,9 +17,6 @@ import com.epam.aidial.core.storage.service.ResourceService;
 import io.vertx.core.json.JsonObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
@@ -28,7 +25,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -94,59 +90,6 @@ public class AccessServiceTest {
         Map<ResourceDescriptor, Set<ResourceAccessType>> result = AccessService.getAppResourceAccess(Set.of(descriptor), context);
 
         assertTrue(result.isEmpty());
-    }
-
-    static Stream<Arguments> hiddenFolderTestCases() {
-        return Stream.of(
-                Arguments.of(".quick_app_name_0.0.1", true),
-                Arguments.of(".mind_map_name_0.0.2", true),
-                Arguments.of(".hidden_folder", true),
-                Arguments.of("normal_folder", false),
-                Arguments.of("folder.with.dots", false),
-                Arguments.of("", false),
-                Arguments.of("folder_starting_with_dot.", false)
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("hiddenFolderTestCases")
-    void shouldIdentifyHiddenFolders(String folderName, boolean expectedResult) {
-        ResourceDescriptor resource = new ResourceDescriptor(
-                ResourceTypes.FILE,
-                folderName,
-                List.of(),
-                "bucket",
-                "public/",
-                false
-        );
-
-        assertEquals(expectedResult, resource.isHidden());
-    }
-
-    static Stream<Arguments> filesInHiddenFoldersTestCases() {
-        return Stream.of(
-                Arguments.of(List.of(".quick_app_name_0.0.1"), true),
-                Arguments.of(List.of("normal_folder", ".hidden_subfolder"), true),
-                Arguments.of(List.of(".hidden", "subfolder", "nested"), true),
-                Arguments.of(List.of("normal_folder"), false),
-                Arguments.of(List.of("folder1", "folder2", "folder3"), false),
-                Arguments.of(List.of(), false)
-        );
-    }
-
-    @ParameterizedTest
-    @MethodSource("filesInHiddenFoldersTestCases")
-    void shouldIdentifyFilesInHiddenFolders(List<String> parentFolders, boolean expectedResult) {
-        ResourceDescriptor resource = new ResourceDescriptor(
-                ResourceTypes.FILE,
-                "document.json",
-                parentFolders,
-                "bucket",
-                "public/",
-                false
-        );
-
-        assertEquals(expectedResult, resource.isHidden());
     }
 
     @Test
