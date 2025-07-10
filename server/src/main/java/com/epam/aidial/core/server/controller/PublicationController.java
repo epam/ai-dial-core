e735a2f74c029e201f82db9d7960681ad0498635
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2025-07-10 11:56:31 +0300

    fix: Need to allow update publications without resources #896

--- server/src/main/java/com/epam/aidial/core/server/controller/PublicationController.java
+++ server/src/main/java/com/epam/aidial/core/server/controller/PublicationController.java
@@ -118,9 +118,6 @@ public class PublicationController {
                     if (!isAdmin()) {
                         throw new HttpException(HttpStatus.FORBIDDEN, "Only admin can update publications");
                     }
-                    if (publication.getResources() == null || publication.getResources().isEmpty()) {
-                        throw new HttpException(HttpStatus.BAD_REQUEST, "Publication must have resources to update");
-                    }
                     return vertx.executeBlocking(() -> publicationService.updatePublication(context, publication), false);
                 })
                 .onSuccess(publication -> context.respond(HttpStatus.OK, publication))
