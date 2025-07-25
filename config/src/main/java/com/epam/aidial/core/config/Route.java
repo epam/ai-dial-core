73e048f6f88d9ba3bb467cf5d1e08369c1104942
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2025-07-25 11:40:58 +0300

    feat: Add support of routes to schema rich applications #923 (#925)

--- config/src/main/java/com/epam/aidial/core/config/Route.java
+++ config/src/main/java/com/epam/aidial/core/config/Route.java
@@ -1,5 +1,6 @@
 package com.epam.aidial.core.config;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.Min;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,21 +15,30 @@ public class Route extends RoleBasedEntity {
 
     public static final AttachmentPath EMPTY_ATTACHMENT_PATHS = new AttachmentPath();
 
+    @JsonAlias({"response", "dial:response"})
     private Response response;
+    @JsonAlias({"rewritePath", "dial:rewritePath"})
     private boolean rewritePath;
+    @JsonAlias({"paths", "dial:paths"})
     private List<Pattern> paths = List.of();
+    @JsonAlias({"methods", "dial:methods"})
     private Set<String> methods = Set.of();
+    @JsonAlias({"upstreams", "dial:upstreams"})
     private List<Upstream> upstreams = List.of();
     /**
      * Indicated max retry attempts to route a single user request.
      */
+    @JsonAlias({"maxRetryAttempts", "dial:maxRetryAttempts"})
     private int maxRetryAttempts = 1;
     /**
      * Determines the order the route is resolved. The lower value means the higher priority.
      */
     @Min(value = 0, message = "Order can't be negative")
+    @JsonAlias({"order", "dial:order"})
     private int order = Integer.MAX_VALUE;
+    @JsonAlias({"permissions", "dial:permissions"})
     private Set<ResourceAccessType> permissions = Set.of();
+    @JsonAlias({"attachmentPaths", "dial:attachmentPaths"})
     private AttachmentPath attachmentPaths = EMPTY_ATTACHMENT_PATHS;
 
     @Data
