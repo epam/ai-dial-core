e1f6991c857dbf1dc3e6827672fa922d0a7ea0d3
Oleksii Klimov <Alexey_Klimov@epam.com>
2025-09-03 16:56:45 +0100

    Fix backward compatibility.

--- server/src/main/java/com/epam/aidial/core/server/data/SharedResource.java
+++ server/src/main/java/com/epam/aidial/core/server/data/SharedResource.java
@@ -1,11 +1,22 @@
 package com.epam.aidial.core.server.data;
 
 import com.epam.aidial.core.config.ResourceAccessType;
+import com.epam.aidial.core.server.util.ProxyUtil;
 import com.epam.aidial.core.storage.data.ShareMetadata;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+import net.minidev.json.annotate.JsonIgnore;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
@@ -20,6 +31,7 @@ public class SharedResource {
     /**
      * The list of users or projects who shared this resource with the current user.
      */
+    @JsonDeserialize(using = SharedByDeserializer.class) // For backward compatibility with string value (action: ignore)
     List<ShareMetadata> sharedBy;
     Set<ResourceAccessType> permissions;
 
@@ -56,5 +68,17 @@ public class SharedResource {
         return permissions == null || permissions.isEmpty()
                 ? withPermissions(EnumSet.copyOf(ResourceAccessType.ALL))
                 : this;
+    }
+
+    public static class SharedByDeserializer extends JsonDeserializer<List<ShareMetadata>> {
+        @Override
+        public List<ShareMetadata> deserialize(JsonParser parser, DeserializationContext context) throws IOException {
+            JsonToken token = parser.currentToken();
+            if (token == JsonToken.START_ARRAY) {
+                return parser.readValueAs(new TypeReference<List<ShareMetadata>>() {
+                });
+            }
+            return null;
+        }
     }
 }
