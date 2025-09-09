501ac41e3321fd646aa31870b23795d59c9b56f4
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2025-09-09 17:01:55 +0300

    fix: Need to update toolset name and reference when create publication request #1037 (#1039)

--- server/src/main/java/com/epam/aidial/core/server/service/ToolSetService.java
+++ server/src/main/java/com/epam/aidial/core/server/service/ToolSetService.java
@@ -24,6 +24,19 @@ public class ToolSetService {
     }
 
     public Pair<ResourceItemMetadata, ToolSet> getToolSet(ProxyContext context, ResourceDescriptor resource, EtagHeader etagHeader) {
+        Pair<ResourceItemMetadata, ToolSet> result = getToolSet(resource, etagHeader);
+        ToolSet toolSet = result.getValue();
+        ResourceItemMetadata meta = result.getKey();
+
+        resourceAuthSettingsService.setResourceAuthStatuses(toolSet.getName(), toolSet.getAuthSettings(), context.getUserSub());
+        toolSet.setAuthor(meta.getAuthor());
+        toolSet.setCreatedAt(meta.getCreatedAt());
+        toolSet.setUpdatedAt(meta.getUpdatedAt());
+
+        return Pair.of(meta, toolSet);
+    }
+
+    private Pair<ResourceItemMetadata, ToolSet> getToolSet(ResourceDescriptor resource, EtagHeader etagHeader) {
         verifyToolSet(resource);
         Pair<ResourceItemMetadata, String> result = resourceService.getResourceWithMetadata(resource, etagHeader);
 
@@ -37,12 +50,6 @@ public class ToolSetService {
         if (toolSet == null) {
             throw new ResourceNotFoundException("ToolSet is not found: " + resource.getUrl());
         }
-
-        resourceAuthSettingsService.setResourceAuthStatuses(toolSet.getName(), toolSet.getAuthSettings(), context.getUserSub());
-        toolSet.setAuthor(meta.getAuthor());
-        toolSet.setCreatedAt(meta.getCreatedAt());
-        toolSet.setUpdatedAt(meta.getUpdatedAt());
-
         return Pair.of(meta, toolSet);
     }
 
@@ -63,6 +70,23 @@ public class ToolSetService {
         });
 
         return Pair.of(meta, toolSet);
+    }
+
+    public void copyToolSet(ResourceDescriptor source, ResourceDescriptor destination, String author, boolean overwrite) {
+        verifyToolSet(source);
+        verifyToolSet(destination);
+
+        Pair<ResourceItemMetadata, ToolSet> result = getToolSet(source, EtagHeader.ANY);
+        ToolSet toolSet = result.getValue();
+        if (author == null) {
+            author = result.getKey().getAuthor();
+        }
+
+        EtagHeader etag = overwrite ? EtagHeader.ANY : EtagHeader.NEW_ONLY;
+        toolSet.setName(destination.getUrl());
+        toolSet.setReference(ProxyUtil.generateReference());
+        String json = ProxyUtil.convertToString(toolSet);
+        resourceService.putResource(destination, json, etag, author);
     }
 
     private static void verifyToolSet(ResourceDescriptor resource) {
