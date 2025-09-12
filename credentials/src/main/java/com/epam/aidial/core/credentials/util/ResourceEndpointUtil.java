5a11cf757db5a7294353a2434df1cd0c6e6a778c
Dmytro Zaichenko <dmytro_zaichenko@epam.com>
2025-09-12 19:32:53 +0300

    fix: fixes metadata url's

--- credentials/src/main/java/com/epam/aidial/core/credentials/util/ResourceEndpointUtil.java
+++ credentials/src/main/java/com/epam/aidial/core/credentials/util/ResourceEndpointUtil.java
@@ -9,7 +9,7 @@ import java.net.URISyntaxException;
 public class ResourceEndpointUtil {
 
     /**
-     * Extracts the base URL (scheme + host) from the given input URL, ignoring the path, query, and fragment components.
+     * Extracts the base URL (scheme + host + port (optional)) from the given input URL, ignoring the path, query, and fragment components.
      *
      * @param url The resource endpoint URL
      * @return The base URL
@@ -25,12 +25,17 @@ public class ResourceEndpointUtil {
 
             String scheme = uri.getScheme();
             String host = uri.getHost();
+            int port = uri.getPort();
 
             if (scheme == null || host == null) {
                 throw new IllegalArgumentException("Invalid URL: Missing scheme or host.");
             }
 
-            return scheme + "://" + host;
+            return String.format("%s://%s%s",
+                    scheme,
+                    host,
+                    (port > 0 ? ":" + port : "")
+            );
 
         } catch (URISyntaxException e) {
             throw new IllegalArgumentException("Invalid URL format: " + url, e);
@@ -40,27 +45,37 @@ public class ResourceEndpointUtil {
     /**
      * Builds the resource metadata endpoint based on a resource endpoint.
      *
-     * @param resourceEndpoint The resource endpoint
+     * @param url The resource endpoint URL
      * @param wellKnownSuffix The well-known URI suffix
      * @return The constructed metadata endpoint.
      */
-    public static String buildMetadataEndpoint(String resourceEndpoint,
+    public static String buildMetadataEndpoint(String url,
                                                String wellKnownSuffix,
                                                boolean ignorePathSuffix) {
+        if (url == null || url.isEmpty()) {
+            throw new IllegalArgumentException("URL cannot be null or empty.");
+        }
+
         try {
-            URI resourceUri = new URI(resourceEndpoint);
+            URI resourceUri = new URI(url);
 
             String scheme = resourceUri.getScheme();
             String host = resourceUri.getHost();
             String path = resourceUri.getPath();
+            int port = resourceUri.getPort();
+
+            if (scheme == null || host == null) {
+                throw new IllegalArgumentException("Invalid URL: Missing scheme or host.");
+            }
 
             if (path.endsWith("/")) {
                 path = path.substring(0, path.length() - 1);
             }
 
-            return String.format("%s://%s/.well-known/%s%s",
+            return String.format("%s://%s%s/.well-known/%s%s",
                     scheme,
                     host,
+                    (port > 0 ? ":" + port : ""),
                     wellKnownSuffix,
                     path.isEmpty() || ignorePathSuffix ? "" : path
             );
