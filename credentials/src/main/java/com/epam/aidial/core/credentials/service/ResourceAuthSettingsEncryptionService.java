1da82c7c023102639db016e4a65e2ef5bee4afd6
Oleksii Donets <oleksii_donets@epam.com>
2025-09-18 17:11:25 +0300

    chore(toolset): add nullability annotation to make code more explicit

--- credentials/src/main/java/com/epam/aidial/core/credentials/service/ResourceAuthSettingsEncryptionService.java
+++ credentials/src/main/java/com/epam/aidial/core/credentials/service/ResourceAuthSettingsEncryptionService.java
@@ -3,15 +3,14 @@ package com.epam.aidial.core.credentials.service;
 import com.epam.aidial.core.config.ResourceAuthSettings;
 import com.epam.aidial.core.credentials.data.credentials.BucketInfo;
 import com.epam.aidial.core.credentials.encryption.CredentialEncryptionService;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.function.Function;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -43,20 +42,20 @@ public class ResourceAuthSettingsEncryptionService {
         String clientSecret = settings.getClientSecret();
         if (clientSecret != null) {
             String processedValue = encrypt
-                    ? encryptValue(bucketInfo, aad, clientSecret)
-                    : decryptValue(bucketInfo, aad, clientSecret);
+                    ? encryptValue(bucketInfo, clientSecret, aad)
+                    : decryptValue(bucketInfo, clientSecret, aad);
             settings.setClientSecret(processedValue);
         }
 
     }
 
-    private String encryptValue(BucketInfo bucketInfo, byte[] aad, String plainText) {
+    private String encryptValue(@Nonnull BucketInfo bucketInfo, @Nonnull String plainText, @Nullable byte[] aad) {
         byte[] plain = plainText.getBytes(UTF_8);
         byte[] encrypted = encryptionService.encrypt(bucketInfo, plain, aad);
         return Base64.getEncoder().encodeToString(encrypted);
     }
 
-    private String decryptValue(BucketInfo bucketInfo, byte[] aad, String encryptedText) {
+    private String decryptValue(@Nonnull BucketInfo bucketInfo, @Nonnull String encryptedText, @Nullable byte[] aad) {
         byte[] encrypted = Base64.getDecoder().decode(encryptedText);
         byte[] plain = encryptionService.decrypt(bucketInfo, encrypted, aad);
         return new String(plain, UTF_8);
