7d4f53298bb2e4679ae95cf8a499acb8925cbebb
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2025-10-28 13:24:01 +0300

    fix: Core fails on unknown property `forwardPerRequestKey` in toolset

--- config/src/main/java/com/epam/aidial/core/config/SecuredResource.java
+++ config/src/main/java/com/epam/aidial/core/config/SecuredResource.java
@@ -1,11 +1,13 @@
 package com.epam.aidial.core.config;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public abstract class SecuredResource extends Deployment {
 
     @JsonAlias({"authSettings", "auth_settings"})
