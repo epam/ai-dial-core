685d0049c37e9f2b1baf33b010fbe8ab009edbf5
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2026-01-21 11:01:17 +0300

    fix: Expiration time is set to a wrong value for invitation link #1315

--- config/src/main/java/com/epam/aidial/core/config/ShareResourceLimit.java
+++ config/src/main/java/com/epam/aidial/core/config/ShareResourceLimit.java
@@ -10,10 +10,18 @@ import lombok.Data;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ShareResourceLimit {
+    /**
+     * The maximum number of users can accept the invitation link.
+     * The default value is unset <code>-1</code>.
+     */
     @JsonAlias({"maxAcceptedUsers", "max_accepted_users"})
-    int maxAcceptedUsers;
+    int maxAcceptedUsers = -1;
+    /**
+     * The time to live of the invitation link is measured in hours.
+     * The default value is unset <code>-1</code>.
+     */
     @JsonAlias({"invitationTtl", "invitation_ttl"})
-    long invitationTtl;
+    long invitationTtl = -1;
 
     public ShareResourceLimit() {
 
@@ -21,11 +29,6 @@ public class ShareResourceLimit {
 
     public ShareResourceLimit(int maxAcceptedUsers, long invitationTtl) {
         this.maxAcceptedUsers = maxAcceptedUsers;
-        this.invitationTtl = invitationTtl;
-    }
-
-    public ShareResourceLimit(long invitationTtl) {
-        this.maxAcceptedUsers = Integer.MAX_VALUE;
         this.invitationTtl = invitationTtl;
     }
 }
