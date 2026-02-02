0ecec20b9a4ae6e50bd49be7eee1eaad5368f5ae
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2026-02-02 17:50:28 +0300

    fix: 403 Error on the flow Core -> AppType/Global Interceptor -> personal application #1323 (#1336)

--- server/src/main/java/com/epam/aidial/core/server/function/BaseRequestFunction.java
+++ server/src/main/java/com/epam/aidial/core/server/function/BaseRequestFunction.java
@@ -1,7 +1,21 @@
 package com.epam.aidial.core.server.function;
 
+import com.epam.aidial.core.config.Application;
+import com.epam.aidial.core.config.ResourceAccessType;
+import com.epam.aidial.core.credentials.data.credentials.CredentialsDescriptor;
+import com.epam.aidial.core.credentials.data.credentials.CredentialsLocator;
 import com.epam.aidial.core.server.Proxy;
 import com.epam.aidial.core.server.ProxyContext;
+import com.epam.aidial.core.server.data.ApiKeyData;
+import com.epam.aidial.core.server.data.AutoSharedData;
+import com.epam.aidial.core.server.security.AccessService;
+import com.epam.aidial.core.server.util.CredentialsLocatorFactory;
+import com.epam.aidial.core.storage.http.HttpException;
+import com.epam.aidial.core.storage.http.HttpStatus;
+import com.epam.aidial.core.storage.resource.ResourceDescriptor;
+import com.epam.aidial.core.storage.resource.ResourceType;
+
+import java.util.List;
 
 public abstract class BaseRequestFunction<T> extends BaseFunction<T, Boolean> {
 
@@ -9,4 +23,62 @@ public abstract class BaseRequestFunction<T> extends BaseFunction<T, Boolean> {
     public BaseRequestFunction(Proxy proxy, ProxyContext context) {
         super(proxy, context);
     }
+
+    void shareApplicationDeployments(Application application) {
+        List<ResourceDescriptor> deployments = proxy.getApplicationSchemaService().getDeployments(application);
+        AccessService accessService = proxy.getAccessService();
+        ApiKeyData destApiKeyData = context.getProxyApiKeyData();
+        for (var deployment : deployments) {
+            if (deployment.isPublic()) {
+                continue;
+            }
+            String resourceUrl = deployment.getUrl();
+            if (accessService.hasReadAccess(deployment, context)) {
+                destApiKeyData.getAttachedDeployments().put(resourceUrl, new AutoSharedData(ResourceAccessType.READ_ONLY));
+                ResourceType resourceType = deployment.getType();
+                attachDeploymentCredentials(accessService, destApiKeyData, resourceUrl, resourceType);
+            } else {
+                throw new HttpException(HttpStatus.FORBIDDEN, "Access denied to the deployment %s".formatted(resourceUrl));
+            }
+        }
+    }
+
+    private void attachDeploymentCredentials(AccessService accessService,
+                                             ApiKeyData destApiKeyData,
+                                             String resourceUrl,
+                                             ResourceType resourceType) {
+        CredentialsLocator credentialsLocator = CredentialsLocatorFactory.fromAnyUrl(resourceUrl, context, resourceType);
+        List<ResourceDescriptor> credentialsResourceDescriptors = credentialsLocator.getUniqueCredentialsDescriptors().stream()
+                .map(CredentialsDescriptor::toResourceDescriptor)
+                .filter(credentialsDescriptor -> accessService.hasReadAccess(credentialsDescriptor, context))
+                .toList();
+        for (ResourceDescriptor credentialsResourceDescriptor : credentialsResourceDescriptors) {
+            destApiKeyData.getAttachedResourceCredentials().put(
+                    credentialsResourceDescriptor.getUrl(),
+                    new AutoSharedData(ResourceAccessType.READ_ONLY));
+        }
+    }
+
+    void shareApplicationFiles(Application application) {
+        List<ResourceDescriptor> files = proxy.getApplicationSchemaService().getFiles(application);
+        ApiKeyData apiKeyData = context.getProxyApiKeyData();
+        for (ResourceDescriptor resource : files) {
+            if (resource.isPublic()) {
+                continue;
+            }
+            String resourceUrl = resource.getUrl();
+            AccessService accessService = proxy.getAccessService();
+            if (accessService.hasReadAccess(resource, context)) {
+                if (resource.isFolder()) {
+                    apiKeyData.getAttachedFolders().put(resourceUrl, new AutoSharedData(ResourceAccessType.READ_ONLY));
+                } else {
+                    apiKeyData.getAttachedFiles().put(resourceUrl, new AutoSharedData(ResourceAccessType.READ_ONLY));
+                }
+            } else {
+                throw new HttpException(HttpStatus.FORBIDDEN, "Access denied to the file %s".formatted(resourceUrl));
+            }
+        }
+    }
+
+
 }
---
+++ server/src/test/java/com/epam/aidial/core/server/function/AutoShareDeploymentFnTest.java
@@ -0,0 +1,101 @@
+package com.epam.aidial.core.server.function;
+
+import com.epam.aidial.core.config.ResourceAccessType;
+import com.epam.aidial.core.server.Proxy;
+import com.epam.aidial.core.server.ProxyContext;
+import com.epam.aidial.core.server.data.ApiKeyData;
+import com.epam.aidial.core.server.security.AccessService;
+import com.epam.aidial.core.server.security.EncryptionService;
+import com.epam.aidial.core.server.service.DeploymentService;
+import com.epam.aidial.core.server.util.ProxyUtil;
+import com.epam.aidial.core.storage.http.HttpException;
+import com.epam.aidial.core.storage.resource.ResourceDescriptor;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+public class AutoShareDeploymentFnTest {
+
+    @Mock
+    private Proxy proxy;
+
+    @Mock
+    private ProxyContext context;
+
+    @Mock
+    private EncryptionService encryptionService;
+
+    @Mock
+    private AccessService accessService;
+
+    @Mock
+    private DeploymentService deploymentService;
+
+    private ApiKeyData proxyApiKeyData;
+
+    @InjectMocks
+    private AutoShareDeploymentFn fn;
+
+    private static final ObjectNode EMPTY_OBJECT = ProxyUtil.MAPPER.createObjectNode();
+
+    @BeforeEach
+    public void beforeEach() {
+        proxyApiKeyData = new ApiKeyData();
+    }
+
+    @Test
+    public void testApply_WhenInitialDeploymentIsNull() {
+        assertFalse(fn.apply(EMPTY_OBJECT));
+        assertTrue(proxyApiKeyData.getAttachedDeployments().isEmpty());
+    }
+
+    @Test
+    public void testApply_WhenInitialDeploymentIsPublicResource() {
+        when(context.getInitialDeployment()).thenReturn("applications/public/my-app");
+
+        assertFalse(fn.apply(EMPTY_OBJECT));
+        assertTrue(proxyApiKeyData.getAttachedDeployments().isEmpty());
+    }
+
+    @Test
+    public void testApply_WhenInitialDeploymentIsForbidden() {
+        when(encryptionService.decrypt(anyString())).thenReturn("/Users/user/");
+        when(proxy.getEncryptionService()).thenReturn(encryptionService);
+        when(proxy.getAccessService()).thenReturn(accessService);
+        when(context.getInitialDeployment()).thenReturn("applications/123/my-app");
+        when(accessService.hasReadAccess(any(ResourceDescriptor.class), eq(context))).thenReturn(false);
+
+        assertThrows(HttpException.class, () -> fn.apply(EMPTY_OBJECT));
+        assertTrue(proxyApiKeyData.getAttachedDeployments().isEmpty());
+    }
+
+    @Test
+    public void testApply_WhenInitialDeploymentIsApp() {
+        when(encryptionService.decrypt(anyString())).thenReturn("/Users/user/");
+        when(proxy.getEncryptionService()).thenReturn(encryptionService);
+        when(proxy.getAccessService()).thenReturn(accessService);
+        when(context.getInitialDeployment()).thenReturn("applications/123/my-app");
+        when(accessService.hasReadAccess(any(ResourceDescriptor.class), eq(context))).thenReturn(true);
+        when(context.getProxyApiKeyData()).thenReturn(proxyApiKeyData);
+        when(proxy.getDeploymentService()).thenReturn(deploymentService);
+
+        assertFalse(fn.apply(EMPTY_OBJECT));
+        assertFalse(proxyApiKeyData.getAttachedDeployments().isEmpty());
+        assertEquals(ResourceAccessType.READ_ONLY, proxyApiKeyData.getAttachedDeployments().get("applications/123/my-app").accessTypes());
+    }
+}
