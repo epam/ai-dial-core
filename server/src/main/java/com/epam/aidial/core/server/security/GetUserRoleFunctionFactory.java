e929bce7d83035ec649f1339505f6dca954d92d6
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2026-02-13 08:34:27 +0300

    fix: Function getGoogleWorkspaceGroups fails with NPE #1355 (#1356)

--- server/src/main/java/com/epam/aidial/core/server/security/GetUserRoleFunctionFactory.java
+++ server/src/main/java/com/epam/aidial/core/server/security/GetUserRoleFunctionFactory.java
@@ -1,5 +1,6 @@
 package com.epam.aidial.core.server.security;
 
+import com.google.common.annotations.VisibleForTesting;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpClient;
@@ -7,13 +8,16 @@ import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.RequestOptions;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHeaders;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+@Slf4j
 class GetUserRoleFunctionFactory {
 
     private static final String GOOGLE_IAM_GROUPS_ENDPOINT = "https://content-cloudidentity.googleapis.com/v1/groups/-/memberships:searchDirectGroups?query=member_key_id=='%s'";
@@ -45,20 +49,29 @@ class GetUserRoleFunctionFactory {
             response.body().map(body -> {
                 try {
                     JsonObject json = body.toJsonObject();
-                    JsonArray memberships = json.getJsonArray("memberships");
-                    List<String> groups = new ArrayList<>();
-                    for (int i = 0; i < memberships.size(); i++) {
-                        JsonObject membership = memberships.getJsonObject(i);
-                        String group = membership.getJsonObject("groupKey").getString("id");
-                        groups.add(group);
-                    }
+                    List<String> groups = extractGroups(json);
                     promise.complete(groups);
                 } catch (Throwable e) {
+                    log.debug("Exception occurred at processing google group response", e);
                     promise.fail(e);
                 }
                 return null;
             }).onFailure(promise::fail);
         }));
         return promise.future();
+    }
+
+    @VisibleForTesting
+    static List<String> extractGroups(JsonObject response) {
+        JsonArray memberships = response.getJsonArray("memberships");
+        List<String> groups = new ArrayList<>();
+        if (memberships != null) {
+            for (int i = 0; i < memberships.size(); i++) {
+                Optional.ofNullable(memberships.getJsonObject(i))
+                        .map(membership -> membership.getJsonObject("groupKey"))
+                        .map(groupKey -> groupKey.getString("id")).ifPresent(groups::add);
+            }
+        }
+        return groups;
     }
 }
