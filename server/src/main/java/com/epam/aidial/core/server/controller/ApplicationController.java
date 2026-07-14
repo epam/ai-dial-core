101081059ff271383bd759f5a333ec03ab263485
Aliaksandr Stsiapanay <aliaksandr_stsiapanay@epam.com>
2026-07-14 16:32:50 +0300

    chore: add missed class import

--- server/src/main/java/com/epam/aidial/core/server/controller/ApplicationController.java
+++ server/src/main/java/com/epam/aidial/core/server/controller/ApplicationController.java
@@ -29,6 +29,7 @@ import io.vertx.core.Future;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
